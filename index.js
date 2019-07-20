ca476b9e3af52abf7b1604b9f1b2c03b23240438
Mouni Mullapudi <mullapudi24@gmail.com>
2019-07-20 04:46:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function switchOnCharmFromTip(tip) {
      return "Thank you so much.";
     case 'not as generous':
      return "Thank you.";
-    
+    default:
       "Bye.";
       break;
   }
